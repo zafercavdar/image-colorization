664e3f558ecb07d6f5dfd7915033babfb600bcf5
zafercavdar <zafercavdar@yahoo.com>
2018-12-31 02:18:59 +0300

    delete reqs, less cores

--- job.sh
+++ job.sh
@@ -3,12 +3,11 @@
 #SBATCH --job-name=cnn-image-colorization
 #SBATCH --gres=gpu:8
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=4
 #SBATCH --partition=mid
 #SBATCH --time=23:59:59
 #SBATCH --output=model.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zcavdar14@ku.edu.tr
 
-pip3 install -r requirements.txt
 python test.py
