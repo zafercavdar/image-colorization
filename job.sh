3cf0510d50ce813b86a0be53697554d0ecf6dd48
zafercavdar <zafercavdar@yahoo.com>
2018-12-31 01:59:00 +0300

    commented lines are back

--- job.sh
+++ job.sh
@@ -24,14 +24,15 @@
 #      sbatch slurm_test_submit.sh
 
 # -= Resources =-
-SBATCH --job-name=cnn-image-colorization
-SBATCH --gres=gpu:8
-SBATCH --nodes=1
-SBATCH --ntasks-per-node=8
-SBATCH --partition=mid
-SBATCH --time=23:59:59
-SBATCH --output=model.out
-SBATCH --mail-type=ALL
-SBATCH --mail-user=zcavdar14@ku.edu.tr
+#
+# SBATCH --job-name=cnn-image-colorization
+# SBATCH --gres=gpu:8
+# SBATCH --nodes=1
+# SBATCH --ntasks-per-node=8
+# SBATCH --partition=mid
+# SBATCH --time=23:59:59
+# SBATCH --output=model.out
+# SBATCH --mail-type=ALL
+# SBATCH --mail-user=zcavdar14@ku.edu.tr
 
 python run.py
