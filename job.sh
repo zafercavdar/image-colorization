0a06e8a7c5525748d2f8ea7defea946a36ac2f1f
zafercavdar <zafercavdar@yahoo.com>
2018-12-31 01:56:20 +0300

    added gres

--- job.sh
+++ job.sh
@@ -26,8 +26,9 @@
 # -= Resources =-
 #
 SBATCH --job-name=cnn-image-colorization
+SBATCH --gres=gpu:8
 SBATCH --nodes=1
-SBATCH --ntasks-per-node=45
+SBATCH --ntasks-per-node=8
 SBATCH --partition=mid
 SBATCH --time=23:59:59
 SBATCH --output=model.out
