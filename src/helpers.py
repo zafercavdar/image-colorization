997b5b1371f534ef02357226b90fc591c2164be1
zafercavdar <zafercavdar@yahoo.com>
2019-01-13 23:09:52 +0300

    uncomment mathplotlib stuff

--- src/helpers.py
+++ src/helpers.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import torch
 from skimage.color import lab2rgb
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 class RunningAverage(object):
@@ -20,24 +20,24 @@ class RunningAverage(object):
         self.avg = self.sum / self.count
 
 
-# def to_rgb(grayscale_input, ab_input, save_path=None, save_name=None):
-#     '''
-#     Show/save rgb image from grayscale and ab channels
-#     Input save_path in the form {'grayscale': '/path/', 'colorized': '/path/'}
-#     '''
-#     plt.clf()  # clear matplotlib
-#     color_image = torch.cat((grayscale_input, ab_input),
-#                             0).numpy()  # combine channels
-#     color_image = color_image.transpose((1, 2, 0))  # rescale for matplotlib
-#     color_image[:, :, 0:1] = color_image[:, :, 0:1] * 100
-#     color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128
-#     color_image = lab2rgb(color_image.astype(np.float64))
-#     grayscale_input = grayscale_input.squeeze().numpy()
-#     if save_path is not None and save_name is not None:
-#         plt.imsave(arr=grayscale_input, fname='{}{}'.format(
-#             save_path['grayscale'], save_name), cmap='gray')
-#         plt.imsave(arr=color_image, fname='{}{}'.format(
-#             save_path['colorized'], save_name))
+def to_rgb(grayscale_input, ab_input, save_path=None, save_name=None):
+    '''
+    Show/save rgb image from grayscale and ab channels
+    Input save_path in the form {'grayscale': '/path/', 'colorized': '/path/'}
+    '''
+    plt.clf()  # clear matplotlib
+    color_image = torch.cat((grayscale_input, ab_input),
+                            0).numpy()  # combine channels
+    color_image = color_image.transpose((1, 2, 0))  # rescale for matplotlib
+    color_image[:, :, 0:1] = color_image[:, :, 0:1] * 100
+    color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128
+    color_image = lab2rgb(color_image.astype(np.float64))
+    grayscale_input = grayscale_input.squeeze().numpy()
+    if save_path is not None and save_name is not None:
+        plt.imsave(arr=grayscale_input, fname='{}{}'.format(
+            save_path['grayscale'], save_name), cmap='gray')
+        plt.imsave(arr=color_image, fname='{}{}'.format(
+            save_path['colorized'], save_name))
 
 
 def is_gpu_available():
